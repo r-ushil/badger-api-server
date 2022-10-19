0749a47489fbfd1e3dcea262c2a493d09af8d48b
Oli Callaghan <oli.callaghan@cub3.com>
2022-10-19 17:52:18 +0000

    feat: define ActivityService and DrillService skeleton [SEGP-48]

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -10,24 +9,68 @@ import (
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 
-	examplev1 "badger-api/gen/example/v1"
-	"badger-api/gen/example/v1/examplev1connect"
+	activity_v1 "badger-api/gen/activity/v1"
+	"badger-api/gen/activity/v1/activity_v1connect"
+
+	drill_v1 "badger-api/gen/drill/v1"
+	"badger-api/gen/drill/v1/drill_v1connect"
 
 	grpcreflect "github.com/bufbuild/connect-grpcreflect-go"
 )
 
-type ExampleServer struct{}
+type ActivityServer struct{}
+type DrillServer struct{}
 
-func (s *ExampleServer) Example(
+func (s *ActivityServer) GetActivity(
 	ctx context.Context,
-	req *connect.Request[examplev1.ExampleRequest],
-) (*connect.Response[examplev1.ExampleResponse], error) {
+	req *connect.Request[activity_v1.GetActivityRequest],
+) (*connect.Response[activity_v1.GetActivityResponse], error) {
 
 	log.Println("Request headers: ", req.Header())
 
-	res := connect.NewResponse(&examplev1.ExampleResponse{
-		ExampleText: fmt.Sprintf("Hello, %s!", req.Msg.Name),
-	})
+	res := connect.NewResponse(&activity_v1.GetActivityResponse{})
+
+	res.Header().Set("Example-Version", "v1")
+
+	return res, nil
+}
+
+func (s *ActivityServer) GetActivities(
+	ctx context.Context,
+	req *connect.Request[activity_v1.GetActivitiesRequest],
+) (*connect.Response[activity_v1.GetActivitiesResponse], error) {
+
+	log.Println("Request headers: ", req.Header())
+
+	res := connect.NewResponse(&activity_v1.GetActivitiesResponse{})
+
+	res.Header().Set("Example-Version", "v1")
+
+	return res, nil
+}
+
+func (s *DrillServer) GetDrill(
+	ctx context.Context,
+	req *connect.Request[drill_v1.GetDrillRequest],
+) (*connect.Response[drill_v1.GetDrillResponse], error) {
+
+	log.Println("Request headers: ", req.Header())
+
+	res := connect.NewResponse(&drill_v1.GetDrillResponse{})
+
+	res.Header().Set("Example-Version", "v1")
+
+	return res, nil
+}
+
+func (s *DrillServer) GetDrills(
+	ctx context.Context,
+	req *connect.Request[drill_v1.GetDrillsRequest],
+) (*connect.Response[drill_v1.GetDrillsResponse], error) {
+
+	log.Println("Request headers: ", req.Header())
+
+	res := connect.NewResponse(&drill_v1.GetDrillsResponse{})
 
 	res.Header().Set("Example-Version", "v1")
 
@@ -35,15 +78,21 @@ func (s *ExampleServer) Example(
 }
 
 func main() {
-	exampleServer := &ExampleServer{}
 	mux := http.NewServeMux()
 
+	activityServer := &ActivityServer{}
+	drillServer := &DrillServer{}
+
 	reflector := grpcreflect.NewStaticReflector(
-		examplev1connect.ExampleServiceName,
+		activity_v1connect.ActivityServiceName,
+		drill_v1connect.DrillServiceName,
 	)
 
-	path, handler := examplev1connect.NewExampleServiceHandler(exampleServer)
+	path, handler := activity_v1connect.NewActivityServiceHandler(activityServer)
 	mux.Handle(path, handler)
+
+	path2, handler2 := drill_v1connect.NewDrillServiceHandler(drillServer)
+	mux.Handle(path2, handler2)
 
 	mux.Handle(grpcreflect.NewHandlerV1(reflector))
 	mux.Handle(grpcreflect.NewHandlerV1Alpha(reflector))
