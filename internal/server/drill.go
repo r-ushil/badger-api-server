72018d286a21d9948c197743aa06233ac267edfa
OliCallaghan <me@olicallaghan.com>
2022-10-21 06:53:44 +0000

    feat: call GetDrill and GetDrills from RPC endpoints [SEGP-35]

--- internal/server/drill.go
+++ internal/server/drill.go
@@ -9,6 +9,7 @@ import (
 	drill_v1 "badger-api/gen/drill/v1"
 	"badger-api/gen/drill/v1/drillv1connect"
 
+	"badger-api/pkg/drill"
 	"badger-api/pkg/server"
 )
 
@@ -20,8 +21,19 @@ func (s *DrillServer) GetDrill(
 	ctx context.Context,
 	req *connect.Request[drill_v1.GetDrillRequest],
 ) (*connect.Response[drill_v1.GetDrillResponse], error) {
-	res := connect.NewResponse(&drill_v1.GetDrillResponse{})
-	res.Header().Set("Example-Version", "v1")
+	d, err := drill.GetDrill(s.ctx, req.Msg.DrillId)
+
+	if err == drill.ErrNotFound {
+		return nil, connect.NewError(connect.CodeNotFound, err)
+	}
+
+	res := connect.NewResponse(&drill_v1.GetDrillResponse{
+		Drill: &drill_v1.Drill{
+			DrillId:          d.GetId(),
+			DrillName:        d.GetName(),
+			DrillDescription: d.GetDescription(),
+		},
+	})
 
 	return res, nil
 }
@@ -31,7 +43,6 @@ func (s *DrillServer) GetDrillInstructions(
 	req *connect.Request[drill_v1.GetDrillInstructionsRequest],
 ) (*connect.Response[drill_v1.GetDrillInstructionsResponse], error) {
 	res := connect.NewResponse(&drill_v1.GetDrillInstructionsResponse{})
-	res.Header().Set("Example-Version", "v1")
 
 	return res, nil
 }
@@ -40,8 +51,21 @@ func (s *DrillServer) GetDrills(
 	ctx context.Context,
 	req *connect.Request[drill_v1.GetDrillsRequest],
 ) (*connect.Response[drill_v1.GetDrillsResponse], error) {
-	res := connect.NewResponse(&drill_v1.GetDrillsResponse{})
-	res.Header().Set("Example-Version", "v1")
+	ds := drill.GetDrills(s.ctx)
+
+	drills := make([]*drill_v1.DrillOverview, 0, len(ds))
+
+	for _, drill := range ds {
+		drills = append(drills, &drill_v1.DrillOverview{
+			DrillId:          drill.GetId(),
+			DrillName:        drill.GetName(),
+			DrillDescription: drill.GetDescription(),
+		})
+	}
+
+	res := connect.NewResponse(&drill_v1.GetDrillsResponse{
+		Drills: drills,
+	})
 
 	return res, nil
 }
