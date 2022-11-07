f6005752c75e36ed537c8fe008e22032fd15cc54
okarz <oka20@ic.ac.uk>
2022-11-07 23:24:23 +0000

    feat: add integration tests for drills and activities

--- test/integration/pkg/activity/activity_test.go
+++ test/integration/pkg/activity/activity_test.go
@@ -8,7 +8,19 @@ import (
 )
 
 func TestActivity(t *testing.T) {
+
 	g := goblin.Goblin(t)
 
 	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
+
+	g.Describe("ActivityServer", func() {
+		g.It("Should be able to retrieve all activities")
+
+		g.It("Should be able to retrieve an activity from a valid activity id")
+
+		g.It("Should have an error when retrieving an activity from an invalid activity id")
+
+		g.It("Should be able to retrieve all activies within a certain time frame")
+	})
+
 }
