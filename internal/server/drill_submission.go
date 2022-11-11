eed642e3e69fc84d08f3361ebd5a6bfbb6059cda
Evie Wright <efw20@ic.ac.uk>
2022-11-11 16:18:06 +0000

    feat: add calls to drill submission database (*run but untested*)

---
+++ internal/server/drill_submission.go
@@ -0,0 +1,112 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/bufbuild/connect-go"
+
+	drill_submission_v1 "badger-api/gen/drill_submission/v1"
+	"badger-api/gen/drill_submission/v1/drill_submissionv1connect"
+
+	"badger-api/pkg/drill_submission"
+	"badger-api/pkg/server"
+)
+
+type DrillSubmissionServer struct {
+	ctx *server.ServerContext
+}
+
+func (s *DrillSubmissionServer) GetDrillSubmission(
+	ctx context.Context,
+	req *connect.Request[drill_submission_v1.GetDrillSubmissionRequest],
+) (*connect.Response[drill_submission_v1.GetDrillSubmissionResponse], error) {
+	log.Println("Getting drill submission with ID:", req.Msg.DrillSubmissionId)
+
+	d, err := drill_submission.GetDrillSubmission(s.ctx, req.Msg.DrillSubmissionId)
+
+	if err != nil {
+		// TODO: Handle properly
+		log.Println(err)
+		return nil, connect.NewError(connect.CodeUnimplemented, err)
+	}
+
+	res := connect.NewResponse(&drill_submission_v1.GetDrillSubmissionResponse{
+		DrillSubmission: &drill_submission_v1.DrillSubmission{
+			DrillSubmissionId:d.GetId(),
+			UserId:           d.GetUserId(),
+			DrillId:          d.GetDrillId(),
+			BucketUrl:        d.GetBucketUrl(),
+			Timestamp:        d.GetTimestamp(),
+			ProcessingStatus: d.GetProcessingStatus(),
+			DrillScore:       d.GetDrillScore(),
+		},
+	})
+
+	return res, nil
+}
+
+func (s *DrillSubmissionServer) GetDrillSubmissions(
+	ctx context.Context,
+	req *connect.Request[drill_submission_v1.GetDrillSubmissionsRequest],
+) (*connect.Response[drill_submission_v1.GetDrillSubmissionsResponse], error) {
+	dss := drill_submission.GetDrillSubmissions(s.ctx)
+
+	drill_submissions := make([]*drill_submission_v1.DrillSubmission, 0, len(dss))
+
+	for _, d := range dss {
+		drill_submissions = append(drill_submissions, &drill_submission_v1.DrillSubmission{
+			DrillSubmissionId:d.GetId(),
+			UserId:           d.GetUserId(),
+			DrillId:          d.GetDrillId(),
+			BucketUrl:        d.GetBucketUrl(),
+			Timestamp:        d.GetTimestamp(),
+			ProcessingStatus: d.GetProcessingStatus(),
+			DrillScore:       d.GetDrillScore(),
+		})
+	}
+
+	res := connect.NewResponse(&drill_submission_v1.GetDrillSubmissionsResponse{
+		DrillSubmissions: drill_submissions,
+	})
+
+	return res, nil
+}
+
+func (s *DrillSubmissionServer) GetUserDrillSubmissions(
+	ctx context.Context,
+	req *connect.Request[drill_submission_v1.GetUserDrillSubmissionsRequest],
+) (*connect.Response[drill_submission_v1.GetUserDrillSubmissionsResponse], error) {
+	dss := drill_submission.GetUserDrillSubmissions(s.ctx, req.Msg.PersonId)
+
+	drill_submissions := make([]*drill_submission_v1.DrillSubmission, 0, len(dss))
+
+	for _, d := range dss {
+		drill_submissions = append(drill_submissions, &drill_submission_v1.DrillSubmission{
+			DrillSubmissionId:d.GetId(),
+			UserId:           d.GetUserId(),
+			DrillId:          d.GetDrillId(),
+			BucketUrl:        d.GetBucketUrl(),
+			Timestamp:        d.GetTimestamp(),
+			ProcessingStatus: d.GetProcessingStatus(),
+			DrillScore:       d.GetDrillScore(),
+		})
+	}
+
+	res := connect.NewResponse(&drill_submission_v1.GetUserDrillSubmissionsResponse{
+		DrillSubmissions: drill_submissions,
+	})
+
+	return res, nil
+}
+
+func RegisterDrillSubmissionService(mux *http.ServeMux, ctx *server.ServerContext) {
+	server := &DrillSubmissionServer{
+		ctx,
+	}
+
+	path, handler := drill_submissionv1connect.NewDrillSubmissionServiceHandler(server)
+
+	mux.Handle(path, handler)
+}
