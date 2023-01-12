c3133e635d7eab8f15273b173e844e4d6745b4db
Oli Callaghan <me@olicallaghan.com>
2023-01-12 02:43:04 +0000

    feat: add batting, catching and leaderboard services to reflection

--- internal/server/relector.go
+++ internal/server/relector.go
@@ -3,8 +3,11 @@ package server
 import (
 	"net/http"
 
+	batting_drillv1connect "badger-api/gen/batting_drill/v1/batting_drillv1connect"
+	"badger-api/gen/catching_drill/v1/catching_drillv1connect"
 	drill_v1connect "badger-api/gen/drill/v1/drillv1connect"
 	drill_submission_v1connect "badger-api/gen/drill_submission/v1/drill_submissionv1connect"
+	"badger-api/gen/leaderboard/v1/leaderboardv1connect"
 	person_v1connect "badger-api/gen/person/v1/personv1connect"
 
 	grpcreflect "github.com/bufbuild/connect-grpcreflect-go"
@@ -15,6 +18,9 @@ func RegisterReflector(mux *http.ServeMux) {
 		drill_v1connect.DrillServiceName,
 		person_v1connect.PersonServiceName,
 		drill_submission_v1connect.DrillSubmissionServiceName,
+		batting_drillv1connect.BattingDrillServiceName,
+		catching_drillv1connect.CatchingDrillServiceName,
+		leaderboardv1connect.LeaderboardServiceName,
 	)
 
 	mux.Handle(grpcreflect.NewHandlerV1(reflector))
